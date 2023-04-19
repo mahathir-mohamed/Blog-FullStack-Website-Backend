b59cae099dce881048c93dc7ade1af3a971d532c
mahathir mohamed <mahathirmohamed791@gmail.com>
2023-04-20 04:39:11 +0530

    cors * changed

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({origin:"*"}));
+app.use(cors());
 // const fileUpload = require('express-fileupload');
 // app.use(fileUpload()); 
 
