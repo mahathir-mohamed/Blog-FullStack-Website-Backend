8f802da517abb39b7de9678a6b8ad158468f44f1
Mahathir_Mohamed <83574025+mahathir-mohamed@users.noreply.github.com>
2023-04-19 16:14:06 +0530

    cors added

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-    origin: '*'
+    origin: 'https://bloggers-world.onrender.com'
 }));
 // const fileUpload = require('express-fileupload');
 // app.use(fileUpload()); 
