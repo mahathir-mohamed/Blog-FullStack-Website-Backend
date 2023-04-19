6c40e8e9f761588f0ec08622af7207fa7aab5327
mahathir mohamed <mahathirmohamed791@gmail.com>
2023-04-20 05:18:17 +0530

    cors defined

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ejs = require('ejs');
-// var confiq=require('./confiq/config');
+var confiq=require('./confiq/config');
 var cors = require('cors');
 
 
