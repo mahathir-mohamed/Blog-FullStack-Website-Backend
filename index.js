d3b9f14a1766348d05ba3a8446368c0d3ced573f
mahathir mohamed <mahathirmohamed791@gmail.com>
2023-04-20 05:02:22 +0530

    new update

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ejs = require('ejs');
 // var confiq=require('./confiq/config');
-var cors = require('cors');
+// var cors = require('cors');
+
+
 
 
 var indexRouter = require('./routes/index');
@@ -14,6 +16,33 @@ var authRouter = require('./routes/users');
 var CommentRouter = require('./routes/Comment');
 
 var app = express();
+
+app.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +54,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+// app.use(cors());
 // const fileUpload = require('express-fileupload');
 // app.use(fileUpload()); 
 
