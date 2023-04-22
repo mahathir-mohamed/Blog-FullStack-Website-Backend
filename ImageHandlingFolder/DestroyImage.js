b53f50212412045eb6bf8c493ec01c694ec576ac
mahathir mohamed <mahathirmohamed791@gmail.com>
2023-04-22 19:58:37 +0530

    Update user added

--- ImageHandlingFolder/DestroyImage.js
+++ ImageHandlingFolder/DestroyImage.js
@@ -1,41 +1,32 @@
 const User = require("../modals/UserSchema");
 const path = require("path");
 const fs = require('fs');
-const cloudinary2 = require('../ImageHandlingFolder/CloudinaryUpload');
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require('../ImageHandlingFolder/Cloudinary');
+// const cloudinary = require('cloudinary').v2;
 
 const DestroyImage =  async(req,res,next)=>{
-        // console.log(req.params.id);
-        const url = [];
-    if(req.files){
-         const uploader = async (path)=>await cloudinary2.uploads(path,'Images');
-         const files = req.files
-         console.log(req.files); 
-         for(const file of files){
-        const {path}=file
-        const newPath = await uploader(path)
-        url.push(newPath);
-        fs.unlinkSync(path)
-         }
-        }
         const DestroyImage =  User.findOne({_id:req.params.id},async (err,docs)=>{
            if(err){
              res.status(400).json({msg:"Something went wrong"})
            }
            if(docs){
-            if(url.length > 0){
+            if(req.body.Image){
                 // console.log(docs)
                 for(let i=0;i<docs.Image.length;i++){
                     cloudinary.uploader.destroy(docs.Image[i].id)
                 }
-               req.Image = url
+                const uploader = await cloudinary.uploader.upload(req.body.Image,'Images');
+               req.Image = {
+                  url:uploader.secure_url,
+                  id:uploader.public_id
+               };
                console.log(req.Image);
-               next()
+               next()       
             }else{
                 console.log("not image updated"); 
                 req.Image=docs.Image[0]
                 // console.log(req.Image);
-                next()
+                next()  
             }}else{
                 res.status(202).json({msg:"User not found",id:req.params.id})
             }
