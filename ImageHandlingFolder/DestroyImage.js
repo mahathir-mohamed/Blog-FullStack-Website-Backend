49df762fbf2fa7b29b7c8252556d568dd1c5fbcd
mahathir mohamed <mahathirmohamed791@gmail.com>
2023-04-23 21:38:50 +0530

    bug fixed

--- ImageHandlingFolder/DestroyImage.js
+++ ImageHandlingFolder/DestroyImage.js
@@ -5,14 +5,14 @@ const cloudinary = require('../ImageHandlingFolder/Cloudinary');
 // const cloudinary = require('cloudinary').v2;
 
 const DestroyImage =  async(req,res,next)=>{
-    // console.log("hi")
+    console.log(req.files);
         const DestroyImage =  User.findOne({_id:req.params.id},async (err,docs)=>{
            if(err){
              res.status(400).json({msg:"Something went wrong"})
            }
            if(docs){
             if(req.files){
-                // console.log(docs)
+                // console.log(req.files)
                 for(let i=0;i<docs.Image.length;i++){
                     cloudinary.uploader.destroy(docs.Image[i].id)
                 }
