b9816e914b6caf4d96ea3b9ae90a54dc5333b50c
Mahathir_Mohamed <83574025+mahathir-mohamed@users.noreply.github.com>
2023-04-25 10:55:52 +0530

    Uncommented

--- routes/index.js
+++ routes/index.js
@@ -44,25 +44,25 @@ router.post('/Favourite',FavouriteBlog);
 router.get("/MyBlogs/:id",FindMyBlog);
 router.get("/FindPost/:id",FindPost);
 router.get("/RecommentPost/:id",RecommentBlog);
-// router.post("/image-upload",async(req,res)=>{
-//     let sampleFile;
-//   let uploadPath;
+router.post("/image-upload",async(req,res)=>{
+     let sampleFile;
+   let uploadPath;
 
-//   if (!req.files || Object.keys(req.files).length === 0) {
-//     return res.status(400).send('No files were uploaded.');
-//   }
+   if (!req.files || Object.keys(req.files).length === 0) {
+     return res.status(400).send('No files were uploaded.');
+   }
 
-//   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-//   sampleFile = req.files.sampleFile;
-//   uploadPath =   'public/images/' + sampleFile.name;
+   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+   sampleFile = req.files.sampleFile;
+   uploadPath =   'public/images/' + sampleFile.name;
 
-//   // Use the mv() method to place the file somewhere on your server
-//   sampleFile.mv(uploadPath, function(err) {
-//     if (err)
-//       return res.status(500).send(err);
+   // Use the mv() method to place the file somewhere on your server
+   sampleFile.mv(uploadPath, function(err) {
+     if (err)
+       return res.status(500).send(err);
 
-//     res.send('File uploaded!');
-//   }); 
-// })
+     res.send('File uploaded!');
+   }); 
+ })
 
 module.exports = router;
