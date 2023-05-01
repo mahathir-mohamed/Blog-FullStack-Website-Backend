b340ef7af7fb7cf9e82ef5eb1fa48bb94333e86d
mahathir mohamed <mahathirmohamed791@gmail.com>
2023-05-01 18:33:24 +0530

    pagination api added

--- Controllers/Blog Controller/AllPost.js
+++ Controllers/Blog Controller/AllPost.js
@@ -1,10 +1,18 @@
 const BlogSchema = require('../../modals/BlogSchema');
 const AllPost = (req,res)=>{
+   const page = parseInt(req.query.page);
+   const limit = parseInt(req.query.limit);
+
+   const StartIndex = (page-1)*limit;
+   const EndIndex = page*limit;
+
    BlogSchema.find((err,docs)=>{
       if(err){
          console.log(err);
       }else{   
-         res.status(200).send(docs);
+         const result = docs.slice(StartIndex,EndIndex);
+         const totaldocument = docs.length;
+         res.status(200).json({result,TotalPost:totaldocument});
       }
    }).populate('Author');
 }
